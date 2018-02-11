b3e7554e6fba5751a10c157db913b0ded0a73951
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-02-11 19:43:15 +0800

    0211 多加一點介紹

--- example.py
+++ example.py
@@ -1,10 +1,39 @@
 # Source: https://stackoverflow.com/a/3579625/6645399
 
-try:
-    from tkFileDialog import askopenfilename
-except:
-        from filedialog import askopenfilename
+from __future__ import print_function
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+# install package programmatically
+import pip
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import pip
+        pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+install_and_import('docopt')
+from docopt import docopt
+
+# Python 2-3 compatible
+try:
+    from Tkinter import Tk
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    from tkFileDialog import askopenfilename
+    from tkSimpleDialog import askinteger
+except:
+    from tkinter import tk
+    tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    from filedialog import askopenfilename
+    from simpledialog import askinteger
+
+# tkinter GUI
+
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 print(filename)
+
+intresult = askinteger("Ask a integer", "Which number you like? [1-5]",
+                                 minvalue=1, maxvalue=5, initialvalue=3) 
+print(intresult)
